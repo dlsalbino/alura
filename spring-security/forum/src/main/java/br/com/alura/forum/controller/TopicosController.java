5834b140fc9eb5f2bb9a9993120c7ea7a4a272fa
Daniel Albino <daniel.albino@gmail.com>
2021-05-10 20:32:50 -0300

    adding CacheEvict on update and delete flows.

--- spring-security/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ spring-security/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -81,6 +81,7 @@ public class TopicosController {
 
     @PutMapping("/{id}")
     @Transactional
+	@CacheEvict(value = "listarTodos", allEntries = true)
     public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
         Optional<Topico> optional = topicoRepository.findById(id);
         if (optional.isPresent()) {
@@ -93,6 +94,7 @@ public class TopicosController {
 
     @DeleteMapping("/{id}")
     @Transactional
+	@CacheEvict(value = "listarTodos", allEntries = true)
     public ResponseEntity<?> remover(@PathVariable Long id) {
         Optional<Topico> optional = topicoRepository.findById(id);
         if (optional.isPresent()) {
