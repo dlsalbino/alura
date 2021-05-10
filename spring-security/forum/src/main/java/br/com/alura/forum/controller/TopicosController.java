9a2e9f732c7de63efd9699c0e3f2e9666f1fccd7
Daniel Albino <daniel.albino@gmail.com>
2021-05-09 23:50:05 -0300

    creating a default sorting.

--- spring-security/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ spring-security/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,6 +44,7 @@ public class TopicosController {
 	
 	@GetMapping
 	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
+								 @PageableDefault(sort = "id", direction = Sort.Direction.DESC)
 								 Pageable paginacao) {
 
 //		Pageable paginacao = PageRequest.of(pagina, qtde, Sort.Direction.DESC, ordenacao);
