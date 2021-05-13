b627b6eebcea08a1845de740805e1bcbc5b1c86b
Daniel Albino <daniel.albino@gmail.com>
2021-05-13 01:47:29 -0300

    tests

--- spring-security/forum/src/main/java/br/com/alura/forum/config/validacao/SecurityConfigurations.java
+++ spring-security/forum/src/main/java/br/com/alura/forum/config/validacao/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.validacao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,27 +8,41 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+    @Autowired
     private AutenticacaoService autencicacaoService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+//        http.authorizeRequests()
+//                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+//                .anyRequest().authenticated().and().formLogin();
+
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/topicos/*").permitAll()
+                .and()
+                .authorizeRequests().antMatchers(HttpMethod.GET,"/topicos").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated().and().formLogin();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(autencicacaoService);
+        auth.userDetailsService(autencicacaoService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
     }
+
+//    public static void main(String[] args) {
+//        System.out.println(new BCryptPasswordEncoder().encode("123456"));
+//    }
 }
--- spring-security/forum/src/main/resources/data.sql
+++ spring-security/forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$mie0Z9/sEssJUosaiLIGUuZQlFeDS8i4eT9mtTcLPuymBeiZfigZ.');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
