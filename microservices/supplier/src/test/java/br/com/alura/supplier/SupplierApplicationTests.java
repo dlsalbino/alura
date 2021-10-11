51419c7b17fbcde6cb605c503124c13cd71dcef9
Daniel Benedito <daniel.benedito@hotmart.com>
2021-10-11 19:33:56 -0300

    supplier microservice created.

---
+++ microservices/supplier/src/test/java/br/com/alura/supplier/SupplierApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.supplier;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
