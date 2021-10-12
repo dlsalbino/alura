108fa0b98171f299cbc1867825a0c4b25e9d5cb4
Daniel Benedito <daniel.benedito@hotmart.com>
2021-10-11 21:12:09 -0300

    changes on PurchaseService class.

--- microservices/store/src/main/java/br/com/alura/store/service/PurchaseService.java
+++ microservices/store/src/main/java/br/com/alura/store/service/PurchaseService.java
@@ -13,9 +13,9 @@ public class PurchaseService {
     public void performPurchase(PurchaseDto purchaseDto) {
 
         RestTemplate client = new RestTemplate();
-        ResponseEntity<InfoSupplierDto> exchange = client.exchange("http://localhost:8081/info/" + purchaseDto.getAddressDto().getState(),
+        ResponseEntity<InfoSupplierDto> exchange = client.exchange("http://localhost:8081/supplier/info/" + purchaseDto.getAddressDto().getState(),
                 HttpMethod.GET, null, InfoSupplierDto.class);
 
-        System.out.println(exchange);
+        System.out.println(exchange.getBody());
     }
 }
