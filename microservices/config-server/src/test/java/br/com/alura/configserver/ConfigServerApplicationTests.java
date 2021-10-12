b92be50fb7d44eb829a33292f5b45e371c9e2eb1
Daniel Benedito <daniel.benedito@hotmart.com>
2021-10-12 01:04:16 -0300

    config-server application created.

---
+++ microservices/config-server/src/test/java/br/com/alura/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.configserver;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+
+    void contextLoads() {
+    }
+
+}
