a944d1c60c23b54eae848063649851d4bd5933be
Daniel Benedito <daniel.benedito@hotmart.com>
2021-10-12 00:29:21 -0300

    client load balancer ribbon configured.

--- microservices/store/src/main/java/br/com/alura/store/StoreApplication.java
+++ microservices/store/src/main/java/br/com/alura/store/StoreApplication.java
@@ -1,10 +1,20 @@
 package br.com.alura.store;
 
+import ch.qos.logback.core.pattern.util.RegularEscapeUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class StoreApplication {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StoreApplication.class, args);
--- microservices/store/src/main/java/br/com/alura/store/service/PurchaseService.java
+++ microservices/store/src/main/java/br/com/alura/store/service/PurchaseService.java
@@ -2,18 +2,21 @@ package br.com.alura.store.service;
 
 import br.com.alura.store.controller.dto.InfoSupplierDto;
 import br.com.alura.store.controller.dto.PurchaseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RequiredArgsConstructor
 public class PurchaseService {
+
+    private final RestTemplate client;
 
     public void performPurchase(PurchaseDto purchaseDto) {
 
-        RestTemplate client = new RestTemplate();
-        ResponseEntity<InfoSupplierDto> exchange = client.exchange("http://localhost:8081/supplier/info/" + purchaseDto.getAddressDto().getState(),
+        ResponseEntity<InfoSupplierDto> exchange = client.exchange("http://supplier/supplier/info/" + purchaseDto.getAddressDto().getState(),
                 HttpMethod.GET, null, InfoSupplierDto.class);
 
         System.out.println(exchange.getBody());
