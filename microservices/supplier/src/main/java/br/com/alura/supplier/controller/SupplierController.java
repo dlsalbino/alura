bce2a80efb78b46c4ee3c108c71552cabb51c673
Daniel Benedito <daniel.benedito@hotmart.com>
2021-10-12 13:02:54 -0300

    adjust on uri.

--- microservices/supplier/src/main/java/br/com/alura/supplier/controller/SupplierController.java
+++ microservices/supplier/src/main/java/br/com/alura/supplier/controller/SupplierController.java
@@ -5,11 +5,9 @@ import br.com.alura.supplier.service.SupplierService.SupplierService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/supplier")
 @RequiredArgsConstructor
 public class SupplierController {
 
